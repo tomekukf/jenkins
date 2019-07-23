bc95e5abb5487c12ba3e348d4c700ca89916c390
tom <domanskitomasz.95@gmail.com>
2019-07-23 14:31:54 -0400

    fix worksapceroute

--- jenkins/pipeline/jenkins/build/mvn.sh
+++ jenkins/pipeline/jenkins/build/mvn.sh
@@ -2,6 +2,7 @@ echo "********************"
 echo "***** BUILDING *****"
 echo "********************"
 
-WORKSPACE=/var/jenkins_home/workspace/jenkins-pipeline/jenkins/pipeline/
+WORKSPACE=/var/jenkins_home/workspace/jenkins-pipeline/jenkins/pipeline
+
 
 docker run --rm  -v /root/.m2:/root/.m2 -v $WORKSPACE/java-app:/app -w /app maven:3-alpine "$@" 
