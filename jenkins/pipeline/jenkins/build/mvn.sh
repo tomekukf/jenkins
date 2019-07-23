31f93dd62772e8b36577b1a9d9ea44585d5797fc
tomekukf <32357951+tomekukf@users.noreply.github.com>
2019-07-23 21:02:11 +0200

    Update mvn.sh

--- jenkins/pipeline/jenkins/build/mvn.sh
+++ jenkins/pipeline/jenkins/build/mvn.sh
@@ -6,4 +6,4 @@ WORKSPACE=/var/jenkins_home/workspace/jenkins-pipeline/jenkins/pipeline
 
 ls /var/jenkins_home/workspace/jenkins-pipeline/jenkins/pipeline
 ls /var/jenkins_home/workspace/jenkins-pipeline/jenkins/pipeline/java-app
-docker run  -v /var/jenkins_home/workspace/jenkins-pipeline/jenkins/pipeline/java-app:/app -w /app maven:3-alpine "$@" 
+docker run  -v /var/jenkins_home/workspace/jenkins-pipeline/jenkins/pipeline/java-app/pom.xml:/app -w /app maven:3-alpine "$@" 
