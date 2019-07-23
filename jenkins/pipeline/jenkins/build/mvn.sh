d9bc2c9c3d70a33ee3e942ca99e7cb957a90d316
tomekukf <32357951+tomekukf@users.noreply.github.com>
2019-07-23 20:42:51 +0200

    1

--- jenkins/pipeline/jenkins/build/mvn.sh
+++ jenkins/pipeline/jenkins/build/mvn.sh
@@ -4,5 +4,5 @@ echo "********************"
 
 WORKSPACE=/var/jenkins_home/workspace/jenkins-pipeline/jenkins/pipeline
 
-
+ls /var/jenkins_home/workspace/jenkins-pipeline/jenkins/pipeline/java-app
 docker run --rm  -v /root/.m2:/root/.m2 -v $WORKSPACE/java-app:/app -w /app maven:3-alpine "$@" 
